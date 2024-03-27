99c2abca92812a02aaf219f18b20412aae58e758
Saumyaraj8178 <saumyaraj8178@gmail.com>
2024-03-27 23:43:01 +0530

    description added for home and about-me section

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,24 @@
 import { Routes } from '@angular/router';
 import { AboutMeComponent } from './about-me/about-me.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
 
 export const routes: Routes = [
   {
-    path: 'about-me ',
+    path: 'about-me',
     component: AboutMeComponent,
+  },
+  {
+    path: '',
+    component: HomepageComponent,
+  },
+  {
+    path: 'projects',
+    component: ProjectsComponent,
+  },
+  {
+    path: 'contact',
+    component: ContactComponent,
   },
 ];
