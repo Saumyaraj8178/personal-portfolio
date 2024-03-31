76b2282485a217d13163963e305f58cb882b871a
Saumyaraj8178 <saumyaraj8178@gmail.com>
2024-04-01 01:32:30 +0530

    created about-us component

---
+++ lib/ui/src/lib/about-us/about-us.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-about-us',
+  templateUrl: './about-us.component.html',
+  styleUrl: './about-us.component.scss',
+})
+export class AboutUsComponent {}
--- lib/ui/src/lib/portfolio-ui.module.ts
+++ lib/ui/src/lib/portfolio-ui.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainComponent } from './main/main.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [MainComponent],
+  declarations: [MainComponent, AboutUsComponent],
   exports: [MainComponent],
 })
 export class PortfolioUiModule {}
